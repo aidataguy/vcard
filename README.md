511f704118a4ae8ac0c9f2347c28fdb936c9a7c5
Himanshu Patel <himanshu-patel@live.com>
2020-05-09 19:53:44 +0530

    Screen shot added with updated readme
    Signed-off-by: Himanshu Patel <himanshu-patel@live.com>

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 a vcard app
 
-## Getting Started
+###A Vcard App built with Flutter using Material Design Along with Material Icons
 
-This project is a starting point for a Flutter application.
+###ScreenShot
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![Screenshot](/images/screen.png)
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepOrangeAccent,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -24,52 +24,70 @@ class MyApp extends StatelessWidget {
                       color: Colors.white,
                       fontWeight: FontWeight.bold)),
               Text(
-                'Flutter Developer',
+                'PYTHON & FLUTTER DEVELOPER',
                 style: TextStyle(
                     fontSize: 20.0,
                     fontFamily: 'Montserrat',
-                    color: Colors.teal.shade200,
+                    color: Colors.deepOrange.shade200,
                     fontWeight: FontWeight.bold),
               ),
               SizedBox(
                 height: 20.0,
                 width: 150.0,
-                child: Divider(color: Colors.teal.shade100),
+                child: Divider(color: Colors.deepOrange.shade100),
               ),
-              Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      '+91 123 456 789',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Montserrat',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  )),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+                    Icons.phone,
+                    color: Colors.deepOrange,
                   ),
                   title: Text(
-                    'himanshu.patel@live.com',
+                    '+91 123 456 789',
                     style: TextStyle(
-                      color: Colors.teal.shade900,
+                      color: Colors.deepOrange.shade900,
                       fontFamily: 'Montserrat',
                       fontSize: 20.0,
                     ),
                   ),
                 ),
               ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    'himanshu.patel@live.com',
+                    style: TextStyle(
+                      color: Colors.deepOrange.shade900,
+                      fontFamily: 'Montserrat',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              RichText(
+                  maxLines: 4,
+                  text: TextSpan(children: [
+                    WidgetSpan(
+                      child: Icon(
+                        Icons.location_on,
+                        color: Colors.deepOrange.shade200,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Pune, India',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Montserrat',
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ])),
             ],
           ),
         ),
